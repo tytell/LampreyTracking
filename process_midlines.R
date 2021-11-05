c0b84acf75a08e180786d1f9a5948abff7fc0d43
Eric Tytell <eric.tytell@tufts.edu>
2021-11-05 11:55:09 +0100

    Finalized kinematics calculations for one file.
    Also added a bunch of comments

--- process_midlines.R
+++ process_midlines.R
@@ -1,6 +1,8 @@
 require(tidyverse)
 
 get_arc_length <- function(df) {
+  #' Calculates arc length along the body
+  
   df %>%
     group_by(frame) %>%
     mutate(dx = lead(xmm) - xmm,
@@ -11,6 +13,9 @@ get_arc_length <- function(df) {
 }
 
 get_length <- function(df) {
+  #' Pulls out the last value of arc length in each frame and uses that to
+  #' estimate the median body length across all frames
+
   df %>%
     group_by(frame) %>%
     summarize(len = last(s)) %>%
@@ -53,46 +58,78 @@ get_center_of_mass <- function(df) {
     left_join(com, by = "frame")
 }
 
-smooth_com_spline <- function(x,y, spar) {
-  ys <- numeric(length(y))
-  good <- !is.na(y)
+smooth_com_spline <- function(t,com, spar) {
+  #' Smooths the center of mass location using a smoothing spline
+  #' 
+  #' @param t Time
+  #' @param com Location of COM (in x or y)
+  #' @param spar Smoothing parameter. Seems to work well with a value of 0.8
   
-  sp <- smooth.spline(x[good], y[good], spar = spar)
+  coms <- numeric(length(com))
+  good <- !is.na(com)
   
-  ys[!good] <- NA
-  ys[good] = predict(sp)$y
+  sp <- smooth.spline(t[good], com[good], spar = spar)
   
-  ys
+  coms[!good] <- NA
+  coms[good] = predict(sp)$y
+  
+  coms
 }
 
 get_swim_vel_dir <- function(df, s = 0.8, fps = 60) {
+  #' Computes the swimming velocity and a smoothed swimming direction vector.
+  #' The swimming direction vector is used later on for calculating amplitudes
+  #' and excursions.
+  #' 
+  #' @param df Data frame
+  #' @param s Smoothing parameter. Seems to work will around 0.8
+  #' @param fps Frames per second
+  
   swim <-
     df %>%
     group_by(frame) %>%
+    # COM has one location per frame, but we have multiple body positions, so we
+    # just take the first value
     summarize(comx = first(comx),
               comy = first(comy),
               t = first(t)) %>%
-    mutate(swimvelx = (lead(comx) - lag(comx)) * fps / 2,
+    mutate(swimvelx = (lead(comx) - lag(comx)) * fps / 2,   # central difference derivative
            swimvely = (lead(comy) - lag(comy)) * fps / 2,
-           swimvel = sqrt(swimvelx^2 + swimvely^2),
-           swimvelxs = smooth_com_spline(t, swimvelx, s),
+           swimvel = sqrt(swimvelx^2 + swimvely^2),  # magnitude is speed
+           swimvelxs = smooth_com_spline(t, swimvelx, s),    # smooth the COM x location
            swimvelys = smooth_com_spline(t, swimvely, s),
            swimvels = sqrt(swimvelxs^2 + swimvelys^2),
-           swimdirx = swimvelxs / swimvels,
+           swimdirx = swimvelxs / swimvels,   # make a normal vector corresponding to the swimming direction
            swimdiry = swimvelys / swimvels) %>%
     select(-comx, -comy, -t)
   
+  # merge back up with the main data frame
   df %>%
     left_join(swim, by = c("frame"))
 }
 
 get_excursions <- function(df) {
+  #' Gets lateral excursion of the body, relative to the swimming direction.
+  #' 
+  #' Projects the position of each body segment, relative to the center of mass, on
+  #' to the vector perpendicular to the swimming direction
   df %>%
     mutate(exc = -(xmm - comx) * swimdiry + 
              (ymm - comy) * swimdirx)
 }
 
 get_cycles <- function(df) {
+  #' Uses the time series for lateral excursion to find the cycle periods
+  #' 
+  #' For each body segment, it looks for where the excursion crosses the swimming
+  #' direction (a zero crossing). Uses linear interpolation to estimate the actual
+  #' time of the zero crossing.
+  #' 
+  #' Then uses those times to estimate the period and frequency of the movement
+  #' at each body segment.
+  #' 
+  #' Finally, estimates a cycle phase for for the tail, which we use as an
+  #' overall phase estimate.
   df <-
     df %>%
     group_by(bodyparts) %>%
@@ -123,13 +160,6 @@ get_cycles <- function(df) {
     select(t0, freqbody) %>%
     mutate(phtail = seq(from = 0, length.out = n())/2)
 
-  t0body <-
-    df %>%
-    ungroup() %>%
-    filter(zerocross != 0 &
-             bodyparts != "tailtip") %>%
-    group_by(bodyparts) %>%
-    mutate()
   df %>%
     ungroup() %>%
     mutate(freq = approx(t0tail$t0, t0tail$freqbody, t)$y,
@@ -138,6 +168,11 @@ get_cycles <- function(df) {
 }
 
 get_amplitudes <- function(df) {
+  #' Computes body undulation amplitude.
+  #' 
+  #' Saves the maximum absolute amplitude in each half cycle and the
+  #' frame at which it occurs. Then smooths the amplitude using the 
+  #' two amplitude estimates on either side.
   amps <-
     df %>%
     group_by(bodyparts, cycle) %>%
@@ -150,6 +185,9 @@ get_amplitudes <- function(df) {
 }
 
 get_next_level <- function(fct) {
+  #' Gets the next level in an ordered factor.
+  #' 
+  #' Returns NA if you try to get the next level after the last one
   lev <- levels(fct)
   n = as.numeric(fct) + 1
   
@@ -161,29 +199,27 @@ get_next_level <- function(fct) {
   nextfct
 }
 
-get_next_t0 <- function(a,b, sgn, nextdf) {
-  nextt0 <- 
-    nextdf %>%
-    filter((t0 >= a) & (t0 < b) & (zerocross == sgn)) %>%
-    transmute(t0nextseg = t0,
-           snextseg = s)
-  
-  if (nrow(nextt0) == 0) {
-    nextt0 = data.frame(t0nextseg = NA, snextseg = NA)
-  }
-  nextt0[1,]
-}
-
 get_wavespeed <- function(df) {
+  #' Estimates the wavespeed from the swimming kinematics.
+  #' 
+  #' Looks at each segment and chooses the time of the current zero crossing
+  #' and the next zero crossing. Then looks at the next segment along the body and
+  #' finds the zero crossing in that segment that has the same sign as the current
+  #' zero crossing. The wave speed is the arc length between the two segments
+  #' divided by the difference in time of the zero crossings.
+ 
   bodyparts <- levels(df$bodyparts)
   bodyparts <- factor(bodyparts, levels = bodyparts)
 
+  # run through the segments
   t0seg = list()
   for (i in seq(length.out = length(bodyparts)-1)) {
     seg1 <- bodyparts[i]
     
     seg2 = get_next_level(seg1)
     
+    # for this segment, save the time of the next zero crossing
+    # as a new column
     t0seg1 <- data1 %>%
       ungroup() %>%
       filter(bodyparts == seg1 & !is.na(t0)) %>%
@@ -191,31 +227,76 @@ get_wavespeed <- function(df) {
       mutate(t0next = lead(t0)) %>%
       select(bodyparts, t0, t0next, zerocross)
     
+    # for the next segment, save the arc length of the segment and
+    # the times of the zero crossings
     t0seg2 = data1 %>%
       ungroup() %>%
       filter(bodyparts == seg2 & !is.na(t0)) %>%
       select(bodyparts, s, t0, zerocross)
     
+    # then run through all of the zero crossings in the current segment
     t0seg1$t0nextseg <- NA
     t0seg1$snextseg <- NA
     for (r in seq(length.out = nrow(t0seg1))) {
-        nextt0 <- 
-          t0seg2 %>%
-          filter((t0 >= t0seg1$t0[r]) & (t0 < t0seg1$t0next[r]) & (zerocross == t0seg1$zerocross[r])) %>%
-          transmute(t0nextseg = t0,
-                    snextseg = s) %>%
-          filter(!is.na(t0nextseg))
+      # in the next segment, look for a zero crossing that's after the one
+      # in the current segment, but before the next one in the current segment, and
+      # has the same sign zero crossing.
+      nextt0 <- 
+        t0seg2 %>%
+        filter((t0 >= t0seg1$t0[r]) & (t0 < t0seg1$t0next[r]) & (zerocross == t0seg1$zerocross[r])) %>%
+        transmute(t0nextseg = t0,
+                  snextseg = s) %>%
+        filter(!is.na(t0nextseg))
       
+
+      # save out the times of the next segment zero crossing and the arc length of the next segment
       t0seg1$t0nextseg[r] <- pull(nextt0, t0nextseg) %>% first()
       t0seg1$snextseg[r] <- pull(nextt0, snextseg) %>% first()
     }
     
+    # also save the name of the next segment
     t0seg1$nextseg = seg2
     t0seg[[i]] = t0seg1
   }
   
+  # stack all of the zero crossing data
   t0seg <- bind_rows(t0seg)
   
+  # and merge it in with the main data set
   df %>%
     left_join(t0seg, by = c("bodyparts", "t0", "zerocross"))
+}
+
+get_wavelength <- function(df) {
+  #' Get the wavelength along the body.
+  #' 
+  #' For each frame, look for nodes (points along the body where the excursion
+  #' goes from one sign to the other). We have relatively few segments, so it
+  #' uses a linear interpolation to find the actual location of the node.
+  #' 
+  #' The wavelength is twice the difference in arc length between two nodes of opposite
+  #' sign along the body. (or just the difference in arc length between two nodes of the
+  #' same sign, but it's rare that we would have that)
+  df <-
+    df %>%
+    group_by(frame) %>%
+    arrange(bodyparts, .by_group = TRUE) %>%
+    mutate(excnextseg = lead(exc),
+           snextseg = lead(s),
+           node = case_when(sign(lead(exc)) > sign(exc)   ~   1,
+                            sign(lead(exc)) < sign(exc)   ~   -1,
+                            TRUE   ~   0))
+  
+  nodes <-
+    df %>%
+    group_by(frame) %>%
+    filter(node != 0) %>%
+    mutate(s0 = s + (snextseg - s) / (excnextseg - exc) * (0 - exc),
+           wavelength = lead(s0) - s0,
+           wavelength = case_when(sign(lead(node)) != sign(node)   ~   wavelength*2,
+                                  sign(lead(node)) == sign(node)   ~   wavelength)) %>%
+    select(frame, bodyparts, s0, wavelength)
+  
+  df %>%
+    left_join(nodes, by = c("frame", "bodyparts"))
 }
